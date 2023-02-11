57f6dbb49dcb273d6f02c2775f41701e2272e6b1
Param-Shah <95398183+Param-Shah@users.noreply.github.com>
2023-02-11 15:05:58 +0530

    Add files via upload

---
+++ chatroom.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCmzHUbVcipLP4fT6OfYFkkg1UB0O9ZZ_g",
+    authDomain: "letschat-917b0.firebaseapp.com",
+    projectId: "letschat-917b0",
+    storageBucket: "letschat-917b0.appspot.com",
+    messagingSenderId: "516378242401",
+    appId: "1:516378242401:web:5a4194d6f464933db9e920",
+    measurementId: "G-3E0899E4RZ"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
